2608489e59fa4ac103418f7e86180290832eca0d
ccod <72893967+charlesdev771@users.noreply.github.com>
2022-12-23 22:24:25 -0300

    Update entrada.asm

--- src/entrada.asm
+++ src/entrada.asm
@@ -1,15 +1,15 @@
 segment  .data
-         LF           equ 0xA                               ; Line Feed
-         NULL         equ 0xD                               ; Final da String
-         SYS_CALL     equ 0x80                              ; Envia informacao ao SO
+         LF           equ 0xA                               
+         NULL         equ 0xD                               
+         SYS_CALL     equ 0x80                              
   ; EAX
-         SYS_EXIT     equ 0x1                               ; Codigo de chamada para finalizar
-         SYS_READ     equ 0x3                               ; Operacao de leitura
-         SYS_WRITE    equ 0x4                               ; Operacao de escrita
+         SYS_EXIT     equ 0x1                               
+         SYS_READ     equ 0x3                               
+         SYS_WRITE    equ 0x4                               
   ; EBX
-         RET_EXIT     equ 0x0                               ; Operacao realizada com Sucesso
-         STD_IN       equ 0x0                               ; Entrada padrao
-         STD_OUT      equ 0x1                               ; Saida padrao
+         RET_EXIT     equ 0x0                             
+         STD_IN       equ 0x0                               
+         STD_OUT      equ 0x1                               
 
 section  .data
          msg          db "Entre com seu nome:", LF, NULL
